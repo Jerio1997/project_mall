7a39e436d7c93c73630d3344bf4d852bfc0497c6
Helios1102 <Helios1102@163.com>
2019-11-16 16:23:24 +0800

    运费配置功能，没写数据验证

---
+++ admin/src/main/java/com/cskaoyan/mall/bean/Express.java
@@ -0,0 +1,9 @@
+package com.cskaoyan.mall.bean;
+
+import lombok.Data;
+
+@Data
+public class Express {
+    private String litemall_express_freight_min;
+    private String litemall_express_freight_value;
+}
