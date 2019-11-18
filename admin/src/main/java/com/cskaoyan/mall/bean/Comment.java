f3d9a33770cdb5fce884695544c639e644362b34
wzwzwz214 <wzwzwz214@gmail.com>
2019-11-18 13:53:49 +0800

    修复comment的时间格式

--- admin/src/main/java/com/cskaoyan/mall/bean/Comment.java
+++ admin/src/main/java/com/cskaoyan/mall/bean/Comment.java
@@ -1,5 +1,7 @@
 package com.cskaoyan.mall.bean;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.util.Date;
 
 public class Comment {
@@ -26,9 +28,9 @@ public class Comment {
     }
 
     private Short star;
-
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date addTime;
-
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date updateTime;
 
     private Boolean deleted;
--- admin/src/main/java/com/cskaoyan/mall/controller/OrderController.java
+++ admin/src/main/java/com/cskaoyan/mall/controller/OrderController.java
@@ -7,6 +7,7 @@ import com.cskaoyan.mall.bean.User;
 import com.cskaoyan.mall.service.OrderService;
 import com.cskaoyan.mall.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
