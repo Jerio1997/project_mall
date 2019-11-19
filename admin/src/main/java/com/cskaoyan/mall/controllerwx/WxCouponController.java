8eb72d55c5046fd2a45d9546f91511d9d13d741b
Jerio1997 <727627621@qq.com>
2019-11-20 00:00:07 +0800

    coupon部分内容

---
+++ admin/src/main/java/com/cskaoyan/mall/controllerwx/WxCouponController.java
@@ -0,0 +1,80 @@
+package com.cskaoyan.mall.controllerwx;
+
+import com.cskaoyan.mall.bean.BaseReqVo;
+import com.cskaoyan.mall.bean.Coupon;
+import com.cskaoyan.mall.service.CouponService;
+import org.mybatis.spring.mapper.MapperScannerConfigurer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Author Jerio
+ * CreateTime 2019/11/19 20:30
+ **/
+
+@RestController
+@RequestMapping("wx/coupon")
+public class WxCouponController {
+
+    @Autowired
+    CouponService couponService;
+
+    @GetMapping("list")
+    public BaseReqVo listCoupon(Integer page, Integer size){
+        BaseReqVo baseReqVo = new BaseReqVo();
+        Map<String,Object> map;
+        map = couponService.queryCouponOnWx(page,size);
+        baseReqVo.setErrmsg("成功");
+        baseReqVo.setErrno(0);
+        baseReqVo.setData(map);
+        return baseReqVo;
+    }
+
+    @PostMapping("receive")
+    public BaseReqVo receiveCoupon(Integer couponId){
+        BaseReqVo baseReqVo = new BaseReqVo();
+        int result = couponService.receiveCoupon(couponId);
+        //没写完呢，还却个userId来完成这部分的逻辑
+        //------------进行user添加券的操作-----
+
+        //-------------操作暂未完成--------
+        baseReqVo.setErrno(0);
+        baseReqVo.setErrmsg("成功");
+        return baseReqVo;
+    }
+
+    @GetMapping("mylist")
+    public BaseReqVo myListCoupon(Integer page, Integer size, Short status){
+        BaseReqVo baseReqVo = new BaseReqVo();
+        Map<String,Object> map = couponService.myListCoupon(page,size,status);
+        baseReqVo.setErrno(0);
+        baseReqVo.setErrmsg("成功");
+        baseReqVo.setData(map);
+        return baseReqVo;
+    }
+
+    @PostMapping("exchange")
+    public BaseReqVo exchangeCoupon(@RequestBody Coupon coupon){
+        String code = coupon.getCode();
+        BaseReqVo baseReqVo = new BaseReqVo();
+        if(code == null){
+            baseReqVo.setErrno(763);
+            baseReqVo.setErrmsg("兑换码不能为空");
+            return baseReqVo;
+        }
+        int result = couponService.exchangeCouponByCode(code);
+        if (result == 1){
+            //表示兑换成功
+            baseReqVo.setErrno(0);
+            baseReqVo.setErrmsg("成功");
+        } else {
+            //表示优惠券兑换码错误
+            baseReqVo.setErrno(742);
+            baseReqVo.setErrmsg("优惠券不正确");
+        }
+        return baseReqVo;
+    }
+}
--- admin/src/main/java/com/cskaoyan/mall/service/CouponServiceImpl.java
+++ admin/src/main/java/com/cskaoyan/mall/service/CouponServiceImpl.java
@@ -3,13 +3,14 @@ package com.cskaoyan.mall.service;
 import com.cskaoyan.mall.bean.*;
 import com.cskaoyan.mall.mapper.CouponMapper;
 import com.cskaoyan.mall.mapper.CouponUserMapper;
+import com.cskaoyan.mall.mapper.UserMapper;
 import com.github.pagehelper.PageHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 /**
  * Author Jerio
@@ -23,6 +24,9 @@ public class CouponServiceImpl implements CouponService{
 
     @Autowired
     CouponUserMapper couponUserMapper;
+
+    @Autowired
+    UserMapper userMapper;
 
     @Override
     public int queryCouponCounts() {
@@ -93,14 +97,7 @@ public class CouponServiceImpl implements CouponService{
         return coupon;
     }
 
-    /*@Override
-    public int queryCouponCountsByCouponId(Integer couponId) {
-        CouponExample example = new CouponExample();
-        CouponExample.Criteria criteria = example.createCriteria();
-        criteria.andIdEqualTo(couponId);
-        long total = couponMapper.countByExample(example);
-        return (int) total;
-    }*/
+
 
     @Override
     public CouponUserListResVo listUserCoupon(Integer page, Integer limit, Integer couponId, Integer userId, Short status, String sort, String order) {
@@ -123,6 +120,80 @@ public class CouponServiceImpl implements CouponService{
         couponUserListResVo.setTotal(total);
         couponUserListResVo.setItems(couponUserList);
         return couponUserListResVo;
+    }
+
+    @Override
+    public Map<String, Object> queryCouponOnWx(Integer page, Integer size) {
+        Map<String,Object> map = new HashMap<>();
+        PageHelper.startPage(page,size);
+        CouponExample example = new CouponExample();
+        CouponExample.Criteria criteria = example.createCriteria();
+        criteria.andDeletedEqualTo(false);
+        List<Coupon> couponList = couponMapper.selectByExample(example);
+        map.put("data",couponList);
+        map.put("count",couponList.size());
+        return map;
+    }
+
+    @Override
+    @Transactional
+    public int receiveCoupon(Integer couponId) {
+        //这里要写获得优惠券的相关sql操作以及逻辑判断
+        //不行，我还得要个UserId
+        return 0;
+    }
+
+    @Override
+    public Map<String, Object> myListCoupon(Integer page, Integer size, Short status) {
+        Map<String,Object> map = new HashMap<>();
+        PageHelper.startPage(page,size);
+        CouponExample example = new CouponExample();
+        CouponExample.Criteria criteria = example.createCriteria();
+        criteria.andDeletedEqualTo(false);
+        criteria.andStatusEqualTo(status);
+        List<Coupon> couponList = couponMapper.selectByExample(example);
+        map.put("data",couponList);
+        map.put("count",couponList.size());
+        return map;
+    }
+
+    /**
+     * 兑换优惠券
+     * @param code
+     * @return  1:表示兑换成功    2:表示优惠券不正确
+     */
+    @Override
+    @Transactional
+    public int exchangeCouponByCode(String code) {//还缺个userId
+        CouponExample example = new CouponExample();
+        CouponExample.Criteria criteria = example.createCriteria();
+        criteria.andDeletedEqualTo(false);
+        short id = 0;
+        criteria.andStatusEqualTo(id);
+        criteria.andCodeEqualTo(code);
+        List<Coupon> couponList = couponMapper.selectByExample(example);
+        if(couponList.size() == 0){
+            //表示没查到
+            return 2;
+        }
+        for (Coupon coupon : couponList) {
+            //-------先对优惠券的数量的操作------------
+            //能够查到的都是未删除的、兑换码正确、券状态为可用的 券
+            Coupon couponCur = new Coupon();
+            couponCur.setUpdateTime(new Date());
+            if(coupon.getTotal() == 1){
+                //表示只剩下1张券了，要把它删除状态置位
+                couponCur.setDeleted(true);
+            } else {
+                //表示数量还够,更改数量
+                couponCur.setTotal((coupon.getTotal()-1));
+            }
+            couponMapper.updateByPrimaryKeySelective(couponCur);
+            //--------再对user添加相应的优惠券------------
+            /*此处还要个userId*/
+
+        }
+        return 1;
     }
 
 
