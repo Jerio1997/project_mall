924e792db11e5ef19b506d8024f2c04a19116d88
wzwzwz214 <wzwzwz214@gmail.com>
2019-11-19 16:55:29 +0800

    查询品牌的具体信息

--- admin/src/main/java/com/cskaoyan/mall/controllerwx/WxBrandController.java
+++ admin/src/main/java/com/cskaoyan/mall/controllerwx/WxBrandController.java
@@ -2,13 +2,14 @@ package com.cskaoyan.mall.controllerwx;
 
 import com.cskaoyan.mall.bean.BaseReqVo;
 import com.cskaoyan.mall.bean.Brand;
-import com.cskaoyan.mall.bean.BrandListReqVo_Wx;
+import com.cskaoyan.mall.bean.BrandListResVo_Wx;
 import com.cskaoyan.mall.service.BrandService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -25,16 +26,35 @@ public class WxBrandController {
      * @return
      */
     @GetMapping("list")
-    public BaseReqVo<BrandListReqVo_Wx> getBrandList(Integer page, Integer size){
+    public BaseReqVo<BrandListResVo_Wx> getBrandList(Integer page, Integer size){
         Map<String, Object> map = brandService.getBrandList(page, size, null, null, "add_time", "desc");
         Long total = (Long) map.get("total");
         Integer totalPages = Math.toIntExact((total / size) + 1);
         List<Brand> brandList = (List<Brand>) map.get("items");
-        BrandListReqVo_Wx brandListReqVo_wx = new BrandListReqVo_Wx();
-        brandListReqVo_wx.setBrandList(brandList);
-        brandListReqVo_wx.setTotalPages(totalPages);
-        BaseReqVo<BrandListReqVo_Wx> baseReqVo = new BaseReqVo<>();
-        baseReqVo.setData(brandListReqVo_wx);
+        BrandListResVo_Wx brandListResVo_wx = new BrandListResVo_Wx();
+        brandListResVo_wx.setBrandList(brandList);
+        brandListResVo_wx.setTotalPages(totalPages);
+        BaseReqVo<BrandListResVo_Wx> baseReqVo = new BaseReqVo<>();
+        baseReqVo.setData(brandListResVo_wx);
+        baseReqVo.setErrno(0);
+        baseReqVo.setErrmsg("成功");
+        return baseReqVo;
+    }
+
+    /**
+     * 查询品牌的具体信息
+     * @param id
+     * @return
+     */
+    @GetMapping("detail")
+    public BaseReqVo getBrandDetail(Integer id){
+        Map<String, Object> map = brandService.getBrandList(1, 10, id, null, "add_time", "desc");
+        List<Brand> brandList = (List<Brand>) map.get("items");
+        Brand brand = brandList.get(0);
+        BaseReqVo baseReqVo = new BaseReqVo();
+        HashMap<String, Object> brandResVo = new HashMap<>();
+        brandResVo.put("brand",brand);
+        baseReqVo.setData(brandResVo);
         baseReqVo.setErrno(0);
         baseReqVo.setErrmsg("成功");
         return baseReqVo;
